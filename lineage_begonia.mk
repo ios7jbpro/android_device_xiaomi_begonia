e2c5435c487d7df52e943cd9a6c797c35d99e012
TTTT555 <albukerko@gmail.com>
2021-01-25 14:54:10 +0000

    begonia: Add support for ViPER4Android FX and build it
    Viper's Audio is software for various platforms, such as Android and PC, Cat.
    It is used to improve the sound in general, with the help of various sound effects,
    i.e. it is actually a system equalizer.
    
    Reference:
    https://github.com/begonia-crdroid/android_packages_apps_ViPER4AndroidFX
    
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -21,6 +21,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
