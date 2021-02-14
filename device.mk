c8ebd17d76ed394dfc1b90fa954b667ebceb930a
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-14 15:39:46 +0100

    begonia: Import XiaomiParts from sdm845-common
    * From commit beba0fc
    * Kill Dirac and thermal stuff
    
    Change-Id: I35edd8aa2698ba510d94285149d3e7a6c9ec7c52
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- device.mk
+++ device.mk
@@ -88,6 +88,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     SettingsProvider \
     Settings
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
