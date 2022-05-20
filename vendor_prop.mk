c273b9671b3da6fd2eff7dc07b7f1ca213ae19f3
Lucchetto <lucchetto.tie@live.com>
2022-05-20 00:02:40 +0000

    begonia: props: disable phantom process monitoring
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -16,6 +16,7 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     debug.mediatek.game_pq_enable=1 \
     debug.mtk_tflite.target_nnapi=29 \
     persist.sys.binary_xml=false \
+    sys.fflag.override.settings_enable_monitor_phantom_procs=false \
     drm.service.enabled=true \
     init.userspace_reboot.is_supported=true \
     media.stagefright.thumbnail.prefer_hw_codecs=true \
