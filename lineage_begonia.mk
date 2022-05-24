bbfdd548f63803933723b067deb5090097d49fbc
7Soldier <reg.fm4@gmail.com>
2022-05-24 11:35:34 +0300

    Revert "begonia: Exclude Flipendo"
    This reverts commit fae75d7cce47198259f2e58a952c1b008481fbe6.
    
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -19,7 +19,6 @@ $(call inherit-product, device/redmi/begonia/device.mk)
 # Inherit some common LineageOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_DOESNT_LIKE_FLIPENDO := true
 
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
